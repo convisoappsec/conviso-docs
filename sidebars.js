dcd1b93b9720504d8e86ec2a9a63779e73d6bb34
Daniel <dbermu89@gmail.com>
2021-05-02 10:09:58 -0300

    init of some integrations + sidebar categorization

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,35 @@
 module.exports = {
-  someSidebar: [
+  docs: [
     'index',
     {
-      CLI: ['cli/installation', 'cli/usage'],
-      Integrations: ['integrations/github-actions'],
+      CLI: ['cli/installation', 'cli/usage']
+    },
+    {
+      type: "category",
+      label: "Integrations",
+      items: [
+        {
+          type: "category",
+          label: "CI/CD",
+          items: [
+                //  'integrations/azure-devops',
+                //  'integrations/circleci',
+                  'integrations/github-actions',
+                //  'integrations/gitlab',
+                //  'integrations/jenkins',
+                  ]
+        },
+      /*  {
+          type: "category",
+          label: "Defect/Bug Tracking",
+          items: [
+                  'integrations/jira',
+                  ]
+        },
+      */
+      ]
+    },
+    {
       Onpremises: ['onpremises/overview', 'onpremises/installation', 'onpremises/configuration'],
       Plugins: [],
     }
