6a632269c09848e2c36550bc4afff9c5b797d612
Eduardo Sousa <eduardodeasousa@gmail.com>
2024-01-10 20:31:35 -0300

    - Updating docker image; - Removing unnecessary steps;

--- .github/actions/s3-sync/entrypoint.sh
+++ .github/actions/s3-sync/entrypoint.sh
@@ -36,9 +36,6 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-echo "Install yarn"
-npm install -g yarn
-
 echo "Install dependencies"
 yarn install
 
